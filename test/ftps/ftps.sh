0718af41bd3dde87a1a0ce6c03b2c336e76b802c
Boyd Tammara <btammara@si-p2.kzn.21-school.ru>
2021-02-27 14:00:40 +0300

    ftps четко

--- test/ftps/ftps.sh
+++ test/ftps/ftps.sh
@@ -6,13 +6,14 @@
 #    By: btammara <btammara@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2021/02/16 18:07:50 by btammara          #+#    #+#              #
-#    Updated: 2021/02/26 16:20:56 by btammara         ###   ########.fr        #
+#    Updated: 2021/02/27 13:56:06 by btammara         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/sh
 
 openrc default
+vsftpd /etc/vsftpd/vsftpd.conf
 # mysql_install_db
 # rc-service mariadb start
 # mysql < ./new_db.sql
@@ -24,8 +25,6 @@ openrc default
 # rc-service php-fpm7 stop
 
 # rc-service mariadb stop
-
-tail -F /dev/null
 
 # /usr/bin/supervisord -c /etc/supervisord.conf
 
